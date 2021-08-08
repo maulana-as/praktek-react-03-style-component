70dac96e41b7a7cc543f443a9c0e065c0ebdfb8b
adly.ms@gmail.com <maulanaadly05@gmail.com>
2021-08-09 01:03:45 +0700

    [UPDATE] Delete route selain home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from "./Pages/Home";
-import Test from "./Pages/Test";
-import Profile from "./Pages/Profile";
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/Profile' component={Profile} />
-          <Route path='/Test' component={Test} />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </>
